50a4c798ee73ac88f28218de8905956c3dc015c2
mporogomankadde <tuhairweronnie@gmail.com>
2022-03-01 22:03:13 -0600

    re

--- w2d2/app.js
+++ w2d2/app.js
@@ -4,7 +4,7 @@
 
 /* eslint-disable*/
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-module.exports = { Accumulator, Calculator }; //add all of your function names here that you need for the node mocha tests
+//module.exports = { Accumulator, Calculator }; //add all of your function names here that you need for the node mocha tests
 
 /**
  * 
