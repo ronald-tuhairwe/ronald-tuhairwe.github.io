aeeb3c757addd85199876df3e259f7210f71f014
mporogomankadde <tuhairweronnie@gmail.com>
2022-03-06 17:17:10 -0600

    fin

--- w2d4/methods.js
+++ w2d4/methods.js
@@ -2,7 +2,7 @@
 
 /* eslint-disable*/
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-module.exports = { groupById, unique, filterRangeInPlace, filterRange, Calculator }; //add all of your function names here that you need for the node mocha tests
+//module.exports = { groupById, unique, filterRangeInPlace, filterRange, Calculator }; //add all of your function names here that you need for the node mocha tests
 
 
 function filterRange(arr, a, b) {
--- w2d5/arrayPracticeTests.js
+++ w2d5/arrayPracticeTests.js
@@ -1,14 +1,14 @@
 "use strict";
 /* global assert doubleNums  doubleAges filterEven filterOver10 findEvenNum findEvenAge */
  //comment out the node specific code when going to the browser
-const assert = require("assert");  //always need this with node
-const myExports = require("./arrayPractice.js");  //with node need the name of your file with your functions here
-const doubleNums = myExports.doubleNums;  //do this for all of the functions used in the Mocha tests
-const doubleAges = myExports.doubleAges;
-const filterEven = myExports.filterEven;
-const filterOver10 = myExports.filterOver10;
-const findEvenNum = myExports.findEvenNum;
-const findEvenAge = myExports.findEvenAge;  
+// const assert = require("assert");  //always need this with node
+// const myExports = require("./arrayPractice.js");  //with node need the name of your file with your functions here
+// const doubleNums = myExports.doubleNums;  //do this for all of the functions used in the Mocha tests
+// const doubleAges = myExports.doubleAges;
+// const filterEven = myExports.filterEven;
+// const filterOver10 = myExports.filterOver10;
+// const findEvenNum = myExports.findEvenNum;
+// const findEvenAge = myExports.findEvenAge;  
 
 
 let numArray;
