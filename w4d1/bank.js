d79bce47d33a03432580a0698157a9e52cadea9a
mporogomankadde <tuhairweronnie@gmail.com>
2022-03-15 20:08:49 -0500

    cool

--- w4d1/bank.js
+++ w4d1/bank.js
@@ -4,12 +4,12 @@
 
 /* exports at end of file since exporting an object, which can only be referenced after definition   */
     
-// const acc = require("./account.js");//with node need the name of your file with your functions here
-// const Account = acc.Account;  //do this for all of the functions used in the Mocha tests
-// const chk = require("./checkingaccount.js");
-// const CheckingAccount = chk.CheckingAccount;
-// const sav = require("./savingsaccount.js");
-// const SavingsAccount = sav.SavingsAccount; 
+const acc = require("./account.js");//with node need the name of your file with your functions here
+const Account = acc.Account;  //do this for all of the functions used in the Mocha tests
+const chk = require("./checkingaccount.js");
+const CheckingAccount = chk.CheckingAccount;
+const sav = require("./savingsaccount.js");
+const SavingsAccount = sav.SavingsAccount; 
 
 
 class Bank {
@@ -85,4 +85,4 @@ endOfMonth(){
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser 
 */
-exports.Bank = Bank;
+//exports.Bank = Bank;
