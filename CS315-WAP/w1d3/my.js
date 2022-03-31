6ea9ea66e087ef2b04731f1db16a64aa74a12804
mporogomankadde <tuhairweronnie@gmail.com>
2022-03-30 23:28:55 -0500

    fiin

--- CS315-WAP/w1d3/my.js
+++ CS315-WAP/w1d3/my.js
@@ -1,5 +1,7 @@
 /* eslint-disable*/
 
+/** ********** Output ************ */
+
 let word= document.getElementById("ron");
 
 console.log(word);
@@ -12,10 +14,35 @@ function app(){
     
 }
 
-
+/** ********** Calculator ************ */
 let a= document.getElementById("a");
 let b= document.getElementById("b");
 let res= document.getElementById("result");
  sum=()=>res.innerText=(+a.value + +b.value);
  minus=()=>res.innerText=(+a.value - +b.value);
  mult=()=>res.innerText=(+a.value * +b.value);
+
+
+ 
+/** ********** Inventory ************ */
+
+let prodname=document.getElementById("name");
+let category=document.getElementById("category");
+let qty= document.getElementById("quantity");
+let tabl=document.querySelector("table");
+console.log(Table);
+
+function add(){
+
+    let tr= document.createElement("tr");
+    tabl.append(tr);
+     
+    tr.innerHTML=" <td> " + prodname.value + "</td> <td> "+ category.value + "</td> <td> "+  qty.value + "</td>"
+
+    prodname.value = "";
+    category.value="";
+    qty.value="";
+
+}
+
+
